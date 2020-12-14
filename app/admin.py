2eac0a365de4fb56a53eb5ef41ad78620f2ac3e7
jason <jteague9@gatech.edu>
2020-12-13 18:33:20 -0800

    Updated MatchupAdmin to allow searching and listing by tournament name

--- app/admin.py
+++ app/admin.py
@@ -12,10 +12,16 @@ class TournamentAdmin(admin.ModelAdmin):
 
 @admin.register(Matchup)
 class MatchupAdmin(admin.ModelAdmin):
-    list_display = ("matchup_id", "tournament", "home_player", "away_player", "start_time", "winner")
+    list_display = ("matchup_id", "get_tournament_name", "home_player", "away_player", "start_time", "winner")
     readonly_fields = ("created_at", "modified_at")
-    search_fields = ("matchup_id", "tournament__tournament_id", "home_player", "away_player")
+    search_fields = ("matchup_id", "tournament__tournament_id", "tournament__name", "home_player", "away_player")
     ordering = ("-created_at",)
+
+    def get_tournament_name(self, obj):
+        return obj.tournament.name
+
+    get_tournament_name.short_description = 'tournament name'
+    get_tournament_name.admin_order_field = 'tournament__name'
 
 
 @admin.register(Period)
