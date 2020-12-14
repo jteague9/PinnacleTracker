74e2608be43db876d9844d586fe28e9baf960a28
jason <jteague9@gatech.edu>
2020-12-13 17:47:29 -0800

    Added admin functionality for Tournaments, Matchups, and Periods

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
-from .models import *
+from app.models import Tournament, Matchup, Period, MoneylineRecord
 
-admin.register(Tournament)
-admin.register(Matchup)
-admin.register(Period)
+
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ("tournament_id", "name")
+    readonly_fields = ("created_at", "modified_at")
+    search_fields = ("tournament_id", "name")
+    ordering = ("-created_at",)
+
+
+@admin.register(Matchup)
+class MatchupAdmin(admin.ModelAdmin):
+    list_display = ("matchup_id", "tournament", "home_player", "away_player", "start_time", "winner")
+    readonly_fields = ("created_at", "modified_at")
+    search_fields = ("matchup_id", "tournament", "home_player", "away_player")
+    ordering = ("-created_at",)
+
+
+@admin.register(Period)
+class PeriodAdmin(admin.ModelAdmin):
+    list_display = ("matchup", "period")
+    readonly_fields = ("created_at", "modified_at")
+    search_fields = ("matchup",)
+    ordering = ("-created_at",)
+
+
 admin.register(MoneylineRecord)
